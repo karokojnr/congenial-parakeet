a2941c5482f9b36208ce68e4db142c6e7c124548
Kennedy Karoko <karokojnr@Kennedys-MacBook-Pro.local>
2024-04-02 00:20:11 +0300

    Initial commit :tada:

---
+++ gateway/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+const proxy = require("express-http-proxy");
+
+const app = express();
+app.use(express.json());
+
+app.use('/customer', proxy('http://localhost:8001'));
+app.use('/shopping', proxy('http://localhost:8003'));
+// * products
+app.use('/', proxy('http://localhost:8002'));
+
+app.listen(8000, () => {
+  console.log("Gateway is running on port 8000");
+});
